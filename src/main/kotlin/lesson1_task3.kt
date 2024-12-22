089a011d4e2e6a0a40247a52cf84f75f2bd4003b
booogiman <booogiman@yandex.ru>
2024-12-22 22:28:37 +0300

    KS1-3 Space fly

---
+++ src/main/kotlin/lesson1_task3.kt
@@ -0,0 +1,14 @@
+package org.example
+
+fun main(){
+    val year = 1961
+    var hour = "09"
+    var minute = "07"
+
+    println("Год полёта - $year")
+    println("Время взлета - $hour:$minute")
+    hour = "10"
+    minute = "55"
+    println("Время посадки - $hour:$minute")
+
+}
